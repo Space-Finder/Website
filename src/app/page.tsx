f2532405e4d164574c76a24b2781d2138c774b5f
FusionSid <siddheshadsv@icloud.com>
2024-07-22 10:03:23 +1200

    feat: initial commit - next js boilerplate

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <main>
+            <nav>space finder</nav>
+            <h1>Where learning finds its place</h1>
+            <p>Classroom allocation made simple</p>
+        </main>
+    );
+}
