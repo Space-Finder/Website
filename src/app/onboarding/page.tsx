19aeadeab2f83ae7053b125ca0cc3c1fc4f9ed22
FusionSid <siddheshadsv@icloud.com>
2024-08-08 11:35:04 +1200

    feat: send user to /onboarding if they are new

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const page = () => {
     // Check if kamar is already verified and if so redirect back to dashboard
-    // on dashboard page if not verified, show that 
+    // on dashboard page if not verified, show that
     return <div>Welcome</div>;
 };
 
