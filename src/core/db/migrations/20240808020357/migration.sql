4af54b9fd5871c5026fb2497c0b21a6dd529a6b9
FusionSid <siddheshadsv@icloud.com>
2024-08-08 14:05:02 +1200

    feat: update prisma migrations

---
+++ src/core/db/migrations/20240808020357/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `email_verified` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "email_verified",
+ADD COLUMN     "kamarPassword" TEXT,
+ADD COLUMN     "kamarVerified" BOOLEAN NOT NULL DEFAULT false;
