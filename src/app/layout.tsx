5929b96cca070f67211a203f189155262c658a82
FusionSid <siddheshadsv@icloud.com>
2024-08-15 11:03:49 +1200

    feat: add radix ui and also move background color into tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
 
 import "./globals.css";
+import "@radix-ui/themes/styles.css";
+
 import { auth } from "@/core/lib/auth";
+import { Theme } from "@radix-ui/themes";
 import { all_fonts } from "@/core/lib/fonts";
 import Navbar from "@/core/components/navbar";
 
@@ -11,20 +14,24 @@ export const metadata: Metadata = {
     description: "Classroom Allocation Made Simple",
 };
 
+const BACKGROUND_COLOR = "#f8f3ef";
+
 export default async function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
     const session = await auth();
-    const font_classname = all_fonts.map((font) => font.variable).join(" ");
+    const font_classnames = all_fonts.map((font) => font.variable).join(" ");
 
     return (
         <html lang="en">
-            <body className={font_classname}>
+            <body className={`bg-[${BACKGROUND_COLOR}] ${font_classnames}`}>
                 <SessionProvider session={session}>
-                    <Navbar />
-                    {children}
+                    <Theme hasBackground={false}>
+                        <Navbar />
+                        {children}
+                    </Theme>
                 </SessionProvider>
             </body>
         </html>
