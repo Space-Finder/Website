53f3226032a6e6627181124d261be96664db85a5
FusionSid <siddheshadsv@icloud.com>
2024-08-14 23:01:46 +1200

    feat: working home page Do need to fix the warning though

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,51 +2,64 @@ import { redirect } from "next/navigation";
 
 import { auth, signIn } from "@/core/lib/auth";
 
+const BACKGROUND_IMAGE = "/gradient.png"
+
 export default async function Home() {
     const session = await auth();
     const signedIn = session && session.user !== null;
 
     return (
-        <main className="font-poppins h-[calc(100vh-9rem)]">
-            <div className="flex h-full flex-col items-center justify-between text-center">
-                <div className="flex h-full flex-col items-center justify-center gap-12 text-center">
-                    <h1 className="text-[7rem] leading-snug">
-                        Where{" "}
-                        <span className="text-[#72563E]">
-                            Learn
-                            <span className="relative inline-block">
-                                <span className="relative inline-block h-16 w-2 bg-[#F9BC12]">
-                                    <span className="absolute left-1/2 top-[-0.78rem] h-0 w-0 -translate-x-1/2 transform border-b-[0.8rem] border-l-[0.28rem] border-r-[0.28rem] border-[#64635C] border-l-transparent border-r-transparent"></span>
-                                    <span className="absolute bottom-2 left-0 h-0.5 w-full bg-black"></span>
-                                    <span className="absolute bottom-0 left-0 h-2 w-full bg-[#C85A6C]"></span>
+        <div>
+            <main className="h-[calc(100vh-9rem)] font-poppins">
+                <div className="flex h-full flex-col items-center justify-between text-center">
+                    <div className="flex h-full flex-col items-center justify-center gap-12 text-center drop-shadow-lg">
+                        <h1 className="text-[7rem] leading-snug drop-shadow-lg">
+                            Where{" "}
+                            <span className="text-[#72563E]">
+                                Learn
+                                <span className="relative inline-block">
+                                    <span className="relative inline-block h-16 w-2 bg-[#F9BC12]">
+                                        <span className="absolute left-1/2 top-[-0.78rem] h-0 w-0 -translate-x-1/2 transform border-b-[0.8rem] border-l-[0.28rem] border-r-[0.28rem] border-[#64635C] border-l-transparent border-r-transparent"></span>
+                                        <span className="absolute bottom-2 left-0 h-0.5 w-full bg-black"></span>
+                                        <span className="absolute bottom-0 left-0 h-2 w-full bg-[#C85A6C]"></span>
+                                    </span>
                                 </span>
+                                ng
                             </span>
-                            ng
-                        </span>
-                        <br />
-                        <span className="text-[#12A88C]">Finds</span> its{" "}
-                        <span className="text-[#DA6F6E] underline">place</span>
-                    </h1>
-                    <form
-                        action={async () => {
-                            "use server";
-                            signedIn
-                                ? redirect("/dashboard")
-                                : await signIn("google");
-                        }}
-                    >
-                        <button
-                            type="submit"
-                            className="rounded-xl bg-[#5D7FD6] px-16 py-3 text-2xl text-white"
+                            <br />
+                            <span className="text-[#12A88C]">
+                                Finds
+                            </span> its{" "}
+                            <span className="text-[#DA6F6E] underline">
+                                place
+                            </span>
+                        </h1>
+                        <form
+                            action={async () => {
+                                "use server";
+                                signedIn
+                                    ? redirect("/dashboard")
+                                    : await signIn("google");
+                            }}
                         >
-                            {signedIn ? "Go To Dashboard" : "Login"}
-                        </button>
-                    </form>
+                            <button
+                                type="submit"
+                                className="rounded-xl bg-[#5D7FD6] px-16 py-3 text-2xl text-white"
+                            >
+                                {signedIn ? "Go To Dashboard" : "Login"}
+                            </button>
+                        </form>
+                    </div>
+                    <p className="text-lg uppercase tracking-[0.15em] text-[#72563E]">
+                        Classroom allocation made simple
+                    </p>
                 </div>
-                <p className="text-lg uppercase tracking-[0.15em] text-[#72563E]">
-                    Classroom allocation made simple
-                </p>
-            </div>
-        </main>
+            </main>
+            <style global>{`
+                body {
+                    background:  url(${BACKGROUND_IMAGE});
+                }
+            `}</style>
+        </div>
     );
 }
