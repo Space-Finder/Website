2ef945d770120f8c4c217dca593a682d712d6362
FusionSid <siddheshadsv@icloud.com>
2024-08-25 00:27:47 +1200

    feat: add reset button to error handler

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,26 +1,35 @@
 "use client";
 
+import { startTransition } from "react";
+import { useRouter } from "next/navigation";
+
 export default function GlobalError({
     error,
+    reset,
 }: {
     error: Error & { digest?: string };
+    reset: () => void;
 }) {
+    const router = useRouter();
+    function handleReset() {
+        startTransition(() => {
+            reset();
+            router.refresh();
+        });
+    }
     return (
-        <html>
-            <body>
-                <div className="flex h-full flex-col items-center text-center">
-                    <h1>&quot;Oops&quot; Something went wrong!</h1>
-                    <h2>{error.name}</h2>
-                    <h3>{error.message}</h3>
-                    <form
-                        action={() => {
-                            window.location.href = "/";
-                        }}
-                    >
-                        <button type="submit">Back Home</button>
-                    </form>
-                </div>
-            </body>
-        </html>
+        <div className="flex h-full flex-col items-center text-center">
+            <h1>&quot;Oops&quot; Something went wrong!</h1>
+            <h2>{error.name}</h2>
+            <h3>{error.message}</h3>
+            <form
+                action={() => {
+                    window.location.href = "/";
+                }}
+            >
+                <button type="submit">Back Home</button>
+            </form>
+            <button onClick={handleReset}>Try again</button>
+        </div>
     );
 }
