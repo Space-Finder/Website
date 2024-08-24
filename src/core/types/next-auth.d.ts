b182a4502a0abeb2370d91cb873400e32b685380
FusionSid <siddheshadsv@icloud.com>
2024-08-24 21:23:00 +1200

    fix: type of JWT token User is now correct

--- src/core/types/next-auth.d.ts
+++ src/core/types/next-auth.d.ts
@@ -1,10 +1,12 @@
 import NextAuth, { type DefaultSession } from "next-auth";
+import { type User as DatabaseUser } from "@prisma/client";
 import { DefaultJWT } from "next-auth/jwt";
 
 declare module "next-auth" {
     interface Session extends DefaultSession {
         user: { id: string };
     }
+    interface User extends DatabaseUser {}
 }
 
 declare module "next-auth/jwt" {
