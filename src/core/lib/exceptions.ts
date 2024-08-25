3cab0b751c3ded6a3f0420ec1bc9374fb38e5137
FusionSid <siddheshadsv@icloud.com>
2024-08-25 22:14:39 +1200

    feat: utility func to extract error message

---
+++ src/core/lib/exceptions.ts
@@ -0,0 +1,15 @@
+export const extractErrorMessage = (error: unknown): string => {
+    if (error instanceof Error) {
+        return error.message;
+    }
+
+    if (error && typeof error === "object" && "message" in error) {
+        return String(error.message);
+    }
+
+    if (typeof error === "string") {
+        return error;
+    }
+
+    return "Uh Oh! Something Went Wrong :(";
+};
