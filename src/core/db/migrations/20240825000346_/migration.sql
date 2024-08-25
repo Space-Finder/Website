2248506db74d5c711ca49872cdbe9fb59ae33929
FusionSid <siddheshadsv@icloud.com>
2024-08-25 19:37:37 +1200

    fix: name, email and image will never be null

---
+++ src/core/db/migrations/20240825000346_/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - Made the column `name` on table `users` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `email` on table `users` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `image` on table `users` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "users" ALTER COLUMN "name" SET NOT NULL,
+ALTER COLUMN "email" SET NOT NULL,
+ALTER COLUMN "image" SET NOT NULL;
