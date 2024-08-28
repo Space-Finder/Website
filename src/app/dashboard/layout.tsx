f756d6546053be0bb24ae23eda75e4870974578e
FusionSid <siddheshadsv@icloud.com>
2024-08-29 02:22:04 +1200

    feat: add a little ui for when a student tries to access the page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -10,7 +10,29 @@ const DashboardLayout = async ({
 }>) => {
     const user = await getUser();
     if (!user.isTeacher) {
-        return <>Student Page In Development</>;
+        return (
+            <section className="my-32">
+                <div className="mx-auto w-full max-w-7xl px-4 md:px-5 lg:px-5">
+                    <div className="flex flex-col items-center justify-end gap-6">
+                        <h1 className="font-poppins text-3xl">SpaceFinder</h1>
+                        <h2 className="font-manrope text-center text-5xl font-bold leading-normal text-black md:text-6xl">
+                            Coming Soon...
+                        </h2>
+                        <p className="text-center text-base font-normal leading-relaxed text-black">
+                            The student page is under construction, We are
+                            almost done! <br /> For Inquiries Contact:{" "}
+                            <a
+                                href=""
+                                className="hover:text-gray-500 hover:underline"
+                            >
+                                {" "}
+                                st22209@ormiston.school.nz
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </section>
+        );
     }
 
     return (
