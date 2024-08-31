c4083fc3dfded4ee1135a62c136a567284986c14
FusionSid <siddheshadsv@icloud.com>
2024-08-31 20:36:25 +1200

    feat: put logo in navbar

--- src/core/components/navbar.tsx
+++ src/core/components/navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import Image from "next/image";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { signIn, signOut } from "next-auth/react";
@@ -11,10 +12,19 @@ const Navbar = () => {
 
     return (
         <nav className="border-b-2 border-black">
-            <div className="font-poppins m-6 flex items-center justify-between">
-                <Link href="/" className="text-2xl">
-                    SpaceFinder
-                </Link>
+            <div className="m-6 flex items-center justify-between font-poppins">
+                <div className="flex gap-2">
+                    <Link href="/" className="text-2xl">
+                        SpaceFinder
+                    </Link>
+                    <Image
+                        src="/spacelogo.png"
+                        alt="logo"
+                        width={28}
+                        height={24}
+                        className="h-7 w-auto"
+                    />
+                </div>
                 <form
                     action={async () => {
                         signedIn ? signOut() : signIn("google");
