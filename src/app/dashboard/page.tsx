d71431f127ca8989a81f0b027ab32ed76a37a86a
FusionSid <siddheshadsv@icloud.com>
2024-08-31 21:26:26 +1200

    feat: colorize classes page depending on common colors

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,7 +9,7 @@ const Dashboard = async () => {
 
     const teacher = await prisma.teacher.findUnique({
         where: { userId: s.user.id },
-        include: { user: true, classes: true },
+        include: { user: true, classes: true, common: true },
     });
 
     if (!teacher) {
@@ -28,26 +28,38 @@ const Dashboard = async () => {
 
     return (
         <div className="mt-10">
-            <div className="mx-auto mb-10 w-full items-center text-center font-inter text-3xl font-bold text-[#3b68da]">
-                <h1 className="drop-shadow-sm">
+            <div className="mx-auto mb-10 w-full items-center text-center font-inter text-3xl font-bold">
+                <h1
+                    style={{ color: teacher.common.color || "#3b68da" }}
+                    className="drop-shadow-sm"
+                >
                     {greeting}, {s.user.name.split(" ")[0]}
                 </h1>
             </div>
             <div>
                 {classes.map((c, i) => {
                     const next = findNextPeriod(c.line)!;
+                    const common = commons.find((co) => co.id === c.commonId)!;
+
                     return (
-                        <div key={i} className="m-2 rounded-xl bg-gray-100 p-2">
+                        <div
+                            style={{
+                                backgroundColor: common.color2 || "#f3f4f6",
+                                borderColor: common.color || "#4b5563",
+                            }}
+                            key={i}
+                            className="m-2 rounded-xl border-l-2 p-2"
+                        >
                             <h6 className="mb-1 text-xl font-semibold leading-8 text-black">
                                 {c.name} ({c.code})
                             </h6>
-                            <p className="text-base font-normal text-gray-600">
-                                {
-                                    commons.filter(
-                                        (co) => co.id === c.commonId,
-                                    )[0].name
-                                }{" "}
-                                (Line: {c.line}), Next Class On:{" "}
+                            <p
+                                style={{
+                                    color: common.color || "#4b5563",
+                                }}
+                                className="text-base font-normal text-gray-600"
+                            >
+                                {common.name} (Line: {c.line}), Next Class On:{" "}
                                 {days[next.dayIndex || 0]}{" "}
                                 {formatTime(next.startTime)}
                             </p>
