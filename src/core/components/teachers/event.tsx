217fe53b379addf2753347afd4799d652b84f170
FusionSid <siddheshadsv@icloud.com>
2024-09-04 00:15:09 +1200

    refac: make events component code cleaner

--- src/core/components/teachers/event.tsx
+++ src/core/components/teachers/event.tsx
@@ -3,7 +3,7 @@ import { Course, Common } from "@prisma/client";
 import { formatTime } from "@/core/lib/time";
 
 export const HOURS = 8;
-type Locations = ((string)[][] | null)[];
+type Locations = (string[][] | null)[];
 
 export const createEventFactory = (
     locations: Locations,
@@ -18,16 +18,22 @@ export const createEventFactory = (
             totalHeight,
         );
 
+        const formattedStart = formatTime(event.start);
+        const formattedEnd = formatTime(event.end);
+        const durationString = `${formattedStart} - ${formattedEnd}`;
+
         if (event.type == "break") {
+            const showTime = !event.start.startsWith("10"); // because starts with 10 = morning tea, which is too small to include space to display the time
+
             return (
                 <div
                     style={eventPositionCSS}
                     className="absolute left-0 right-0 z-10 mx-[0.1rem] rounded border-l-2 border-orange-600 bg-orange-50 p-1.5"
                 >
                     <p className="text-xs font-semibold">Break</p>
-                    {!event.start.startsWith("10") && (
+                    {showTime && (
                         <p className="text-xs font-semibold text-orange-600">
-                            {formatTime(event.start)} - {formatTime(event.end)}
+                            {durationString}
                         </p>
                     )}
                 </div>
@@ -35,81 +41,75 @@ export const createEventFactory = (
         }
 
         if (event.type == "custom") {
+            const isLA = event.name === "LA"; // if LA theres no space to display full location
+            const common = isLA
+                ? teacherCommon.name.slice(0, 4).toUpperCase()
+                : teacherCommon.name;
+            const { color, color2 } = teacherCommon;
+
             return (
                 <div
                     style={{
                         ...eventPositionCSS,
-                        borderColor: teacherCommon.color,
-                        backgroundColor: teacherCommon.color2,
+                        borderColor: color,
+                        backgroundColor: color2,
                     }}
                     className="absolute left-0 right-0 z-10 mx-[0.1rem] rounded border-l-2 border-green-600 bg-green-50 p-1.5"
                 >
                     <p className="text-xs font-semibold">{event.name}</p>
-
-                    {event.name !== "LA" ? (
-                        <>
-                            <p
-                                style={{
-                                    color: teacherCommon.color,
-                                }}
-                                className={`mb-px text-xs font-bold`}
-                            >
-                                {teacherCommon.name}
-                            </p>
-                            <p
-                                style={{
-                                    color: teacherCommon.color,
-                                }}
-                                className="text-xs font-semibold"
-                            >
-                                {formatTime(event.start)} -{" "}
-                                {formatTime(event.end)}
-                            </p>
-                        </>
-                    ) : (
-                        <p
-                            style={{
-                                color: teacherCommon.color,
-                            }}
-                            className="text-xs font-semibold"
-                        >
-                            {formatTime(event.start)} - {formatTime(event.end)}{" "}
-                            ({teacherCommon.name.slice(0, 4).toUpperCase()})
-                        </p>
-                    )}
+                    <p
+                        style={{
+                            color,
+                        }}
+                        className="mb-px text-xs font-bold"
+                    >
+                        {isLA ? (
+                            `(${common}) `
+                        ) : (
+                            <>
+                                {common} <br />
+                            </>
+                        )}
+                        {durationString}
+                    </p>
                 </div>
             );
         }
 
-        const event_location_data = locations[event.line - 1];
-        const [location, common, color, color2] =
-            event_location_data && event_location_data[event.periodNumber - 1]
-                ? event_location_data[event.periodNumber - 1]
-                : ["Unset", "Unknown", "", ""];
+        // as indexes start from 0
+        const line = event.line - 1;
+        const period = event.periodNumber - 1;
+
+        const event_location_data = locations[line];
+        const [location, common, primaryColor, secondaryColor] =
+            event_location_data && event_location_data[period]
+                ? event_location_data[period]
+                : ["Unknown", "Unknown", "", ""]; // default location, common...
+
+        const course = lineList[line] || { name: "Unknown", code: "UNKN" };
 
         return (
             <div
                 style={{
                     ...eventPositionCSS,
-                    borderColor: color,
-                    backgroundColor: color2,
+                    borderColor: primaryColor,
+                    backgroundColor: secondaryColor,
                 }}
-                className={`absolute left-0 right-0 z-10 mx-[0.1rem] rounded border-l-2 bg-blue-50 p-1.5`}
+                className="absolute left-0 right-0 z-10 mx-[0.1rem] rounded border-l-2 border-blue-600 bg-blue-50 p-1.5"
             >
                 <p className="text-xs font-semibold">
-                    {lineList[event.line - 1]?.name} - (
-                    {lineList[event.line - 1]?.code})
+                    {course.name} - ({course.code})
                 </p>
                 <p className="mb-px text-xs font-normal text-gray-900">
                     {location} ({common!.slice(0, 4).toUpperCase()})
                 </p>
                 <p
                     style={{
-                        color: color || "#2563eb",
+                        color: primaryColor,
                     }}
                     className="text-xs font-semibold"
                 >
-                    {formatTime(event.start)} - {formatTime(event.end)}
+                    {durationString}
                 </p>
             </div>
         );
