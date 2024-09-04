fddd6d6a76df4993d680fbd381de328863080548
FusionSid <siddheshadsv@icloud.com>
2024-09-04 18:36:51 +1200

    fix: parameters of errors

--- src/core/lib/error.ts
+++ src/core/lib/error.ts
@@ -1,18 +1,14 @@
 export class APIDown extends Error {
-    constructor(cause: any) {
+    constructor() {
         super(
             "There was an issue making a request to the backend API. It looks like the API is down",
         );
-        this.cause = cause;
     }
 }
 
 export class APIRequestError extends Error {
-    reason: string;
-
-    constructor(cause: any, reason: string) {
+    constructor(cause: any) {
         super("There was an issue making a request to the backend API");
-        this.reason = reason;
         this.cause = cause;
     }
 }
