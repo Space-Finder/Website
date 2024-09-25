c40ed8e051ad1a99705a913d9e308d36646dcba7
FusionSid <siddheshadsv@icloud.com>
2024-09-26 10:49:46 +1200

    refac: move auth config to standalone file

---
+++ src/core/auth/config.ts
@@ -0,0 +1,29 @@
+import { AuthConfig } from "@core/types";
+
+const inDevelopmentMode = process.env.NODE_ENV == "development";
+const NEW_USER_URL = inDevelopmentMode ? "/" : "/onboarding";
+
+const AUTH_CONFIG = {
+    pages: {
+        signIn: "/dashboard",
+        signOut: "/",
+        newUser: NEW_USER_URL,
+    },
+    token: {
+        accessTokenLifespan: 60 * 15, // 15 Minutes
+        refreshTokenLifespan: 60 * 60 * 24 * 7, // 7 Days
+    },
+    secrets: {
+        accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+        refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
+        csrfTokenSecret: process.env.CSRF_TOKEN_SECRET,
+    },
+    google: {
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+    authBaseURL: `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth`,
+    errorURL: `${process.env.NEXT_PUBLIC_BASE_URL}/auth/error`,
+} satisfies AuthConfig;
+
+export default AUTH_CONFIG;
--- src/core/auth/index.ts
+++ src/core/auth/index.ts
@@ -5,32 +5,7 @@ import { login, logout } from "./actions";
 import { authRequestHandler } from "./http";
 import { useServerSession } from "./session";
 
-const inDevelopmentMode = process.env.NODE_ENV == "development";
-
-const NEW_USER_URL = inDevelopmentMode ? "/" : "/onboarding";
-
-const config = {
-    pages: {
-        signIn: "/dashboard",
-        signOut: "/",
-        newUser: NEW_USER_URL,
-    },
-    token: {
-        accessTokenLifespan: 60 * 15, // 15 Minutes
-        refreshTokenLifespan: 60 * 60 * 24 * 7, // 7 Days
-    },
-    secrets: {
-        accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
-        refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
-        csrfTokenSecret: process.env.CSRF_TOKEN_SECRET,
-    },
-    google: {
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    },
-    authBaseURL: `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth`,
-    errorURL: `${process.env.NEXT_PUBLIC_BASE_URL}/auth/error`,
-} satisfies AuthConfig;
+import AUTH_CONFIG from "./config";
 
 type handlerParam = { params: { route: string[] } };
 
@@ -48,4 +23,4 @@ function Auth(config: AuthConfig) {
     };
 }
 
-export const { auth, handlers, signIn, signOut } = Auth(config);
+export const { auth, handlers, signIn, signOut } = Auth(AUTH_CONFIG);
