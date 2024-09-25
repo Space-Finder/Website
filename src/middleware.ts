09d8f998aae1d5e61434cf494c841aff95977c13
FusionSid <siddheshadsv@icloud.com>
2024-09-26 10:50:40 +1200

    refac: use already existing auth function for middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,18 @@
-import { decodeJwt, jwtVerify } from "jose";
 import { NextResponse, NextRequest } from "next/server";
 
-import { AccessTokenValidator } from "@lib/validators";
+import AUTH_CONFIG from "@core/auth/config";
+import { useServerSession } from "@core/auth/session";
 
 export async function middleware(request: NextRequest) {
-    const redirectURL = NextResponse.redirect(
-        new URL(`/login?callbackUrl=${request.url}`, request.url),
-    );
-    const secret = new TextEncoder().encode(process.env.ACCESS_TOKEN_SECRET);
-
-    const accessToken = request.cookies.get("accessToken");
-    if (!accessToken || !accessToken.value) {
-        return redirectURL;
-    }
-
-    try {
-        await jwtVerify(accessToken.value, secret);
-    } catch (err) {
-        return redirectURL;
-    }
-
-    const parsedJWT = AccessTokenValidator.safeParse(
-        decodeJwt(accessToken.value),
+    const redirectURL = new URL(
+        `/login?callbackUrl=${request.url}`,
+        request.url,
     );
 
-    const tokenMalformed = parsedJWT.success === false;
-    if (tokenMalformed) {
-        return redirectURL;
+    const session = await useServerSession(AUTH_CONFIG);
+
+    if (!session) {
+        return NextResponse.redirect(redirectURL);
     }
 }
 
