4028cf6585f88c68279775db02eb32fd1c4c845e
FusionSid <siddheshadsv@icloud.com>
2024-09-25 13:52:27 +1200

    feat: working server session function

--- src/core/auth/session.ts
+++ src/core/auth/session.ts
@@ -10,6 +10,50 @@ import { AuthConfig } from "@core/types";
 import { Session } from "@core/types/auth";
 import { AccessTokenValidator } from "@lib/validators";
 
+export async function serverSession(
+    config: AuthConfig,
+): Promise<Session | null> {
+    const accessToken = cookies().get("accessToken");
+    const refreshToken = cookies().get("accessToken");
+
+    if (!accessToken || !accessToken.value) {
+        return refresh(config, refreshToken);
+    }
+
+    try {
+        jwt.verify(accessToken.value, config.secrets.accessTokenSecret);
+    } catch (err) {
+        if (err instanceof TokenExpiredError) {
+            return refresh(config, refreshToken);
+        }
+        return null;
+    }
+
+    const parsedJWT = AccessTokenValidator.safeParse(
+        jwt.decode(accessToken.value),
+    );
+    const tokenMalformed = parsedJWT.success === false;
+
+    if (tokenMalformed) {
+        return refresh(config, refreshToken);
+    }
+
+    const { data } = parsedJWT;
+
+    return {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        image: data.image,
+    };
+}
+
+export function refresh(
+    config: AuthConfig,
+    refreshToken: RequestCookie | undefined,
+) {
+    return null;
+}
 
 export function issueTokens(config: AuthConfig, userData: Session) {
     const { id, name, email, image } = userData;
