7183bd0b5ffcdf14c9b63586238a98f9dac12dce
FusionSid <siddheshadsv@icloud.com>
2024-09-25 23:08:19 +1200

    feat: add authentication middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,33 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { decodeJwt, jwtVerify } from "jose";
+import { NextResponse, NextRequest } from "next/server";
+
+import { AccessTokenValidator } from "@lib/validators";
 
 export async function middleware(request: NextRequest) {
-    return NextResponse.redirect(new URL("/", request.url));
+    const redirectURL = NextResponse.redirect(
+        new URL(`/login?callbackUrl=${request.url}`, request.url),
+    );
+    const secret = new TextEncoder().encode(process.env.ACCESS_TOKEN_SECRET);
+
+    const accessToken = request.cookies.get("accessToken");
+    if (!accessToken || !accessToken.value) {
+        return redirectURL;
+    }
+
+    try {
+        await jwtVerify(accessToken.value, secret);
+    } catch (err) {
+        return redirectURL;
+    }
+
+    const parsedJWT = AccessTokenValidator.safeParse(
+        decodeJwt(accessToken.value),
+    );
+
+    const tokenMalformed = parsedJWT.success === false;
+    if (tokenMalformed) {
+        return redirectURL;
+    }
 }
 
 export const config = {
