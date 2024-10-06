add7d7ec6c3b672dca7cccac884512283538897c
FusionSid <siddheshadsv@icloud.com>
2024-10-07 01:04:22 +1300

    feat: add layout and basic structure for dashboard page

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+import { auth } from "@core/auth";
+import Logo from "/public/spacelogo.png";
+import DashboardSidebar from "@components/dashboard/sidebar";
+
+const DashboardLayout = async ({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) => {
+    const user = await auth();
+
+    if (!user) {
+        return redirect("/login");
+    }
+
+    if (user.role === "STUDENT") {
+        return <StudentMessage />;
+    }
+
+    return (
+        <div className="flex">
+            <DashboardSidebar />
+            <main className="flex-1">{children}</main>
+        </div>
+    );
+};
+
+const StudentMessage = () => {
+    return (
+        <section className="my-32">
+            <div className="mx-auto w-full max-w-7xl px-4 md:px-5 lg:px-5">
+                <div className="flex flex-col items-center justify-end gap-6">
+                    <h1 className="flex items-center gap-2 font-poppins text-3xl">
+                        SpaceFinder{" "}
+                        <Image
+                            src={Logo}
+                            alt="logo"
+                            className="h-7 w-auto"
+                            placeholder="blur"
+                        />
+                    </h1>
+                    <h2 className="font-manrope text-center text-5xl font-bold leading-normal text-black md:text-6xl">
+                        Teachers Only!
+                    </h2>
+                    <p className="text-center text-base font-normal leading-relaxed text-black">
+                        Sorry mate, currently SpaceFinder is only for teachers
+                        to use. <br /> We may make a student portal in the
+                        future. <br /> For Inquiries Contact:{" "}
+                        <a
+                            href=""
+                            className="font-bold underline hover:text-gray-500"
+                        >
+                            {" "}
+                            st22209@ormiston.school.nz
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default DashboardLayout;
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from "react";
+
+import { useSession } from "@core/auth/provider";
+
+const Page = () => {
+    const session = useSession();
+
+    if (session.status !== "authenticated") {
+        return <>loading</>;
+    }
+
+    return (
+        <div>
+            <h1>Test</h1>
+            <p>{session.data.id}</p>
+            <p>{session.data.name}</p>
+            <p>{session.data.role}</p>
+            <p>{session.data.email}</p>
+            <p>{session.data.image}</p>
+            <p>{session.data.expiresAt}</p>
+        </div>
+    );
+};
+
+export default Page;
