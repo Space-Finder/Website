5321418912d1cacbdca0f1eeb39a9278545e202f
FusionSid <siddheshadsv@icloud.com>
2024-10-11 10:52:24 +1300

    feat: add constants for hours in day and start hour

--- src/core/lib/consts.ts
+++ src/core/lib/consts.ts
@@ -1,3 +1,5 @@
 export const SCHOOL_DOMAIN = "ormiston.school.nz";
 export const inDevelopmentMode = process.env.NODE_ENV == "development";
 export const ACCESS_TOKEN_EXPIRY = 15 * 60; // 15 minutes
+export const HOURS_IN_DAY = 9; // 8am - 4pm
+export const START_HOUR = 8; // start timetable at 8am
