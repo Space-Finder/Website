cbdebf0b72274c13e22ea71eeee1cdbb93e65552
FusionSid <siddheshadsv@icloud.com>
2024-10-11 10:52:44 +1300

    feat: add function to convert 24h to 12h

---
+++ src/core/lib/times.ts
@@ -0,0 +1,6 @@
+// converts 24h to 12h time
+export function formatTime(timeString: string) {
+    const [hourString, minute] = timeString.split(":");
+    const hour = +hourString % 24;
+    return (hour % 12 || 12) + ":" + minute + (hour < 12 ? "AM" : "PM");
+}
