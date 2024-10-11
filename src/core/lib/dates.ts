8e6dc1b613b7487e299db8d899b7fb602b9872d3
FusionSid <siddheshadsv@icloud.com>
2024-10-12 01:46:05 +1300

    feat: add inverse function of get week

--- src/core/lib/dates.ts
+++ src/core/lib/dates.ts
@@ -1,13 +1,21 @@
-import { getISOWeek } from "date-fns";
 import { toZonedTime } from "date-fns-tz";
+import { getISOWeek, setISOWeek, startOfISOWeek } from "date-fns";
+
+const TIME_ZONE = "Pacific/Auckland";
 
 export function getWeek(date?: Date): number {
-    const timeZone = "Pacific/Auckland";
-
     if (!date) {
         date = new Date();
     }
 
-    const d = toZonedTime(date, timeZone);
+    const d = toZonedTime(date, TIME_ZONE);
     return getISOWeek(d);
+}
+
+export function getDateFromWeek(year: number, weekNumber: number): Date {
+    let date = new Date(year, 0, 1); // January 1st of the given year
+    date = setISOWeek(date, weekNumber);
+    date = startOfISOWeek(date);
+
+    return toZonedTime(date, TIME_ZONE);
 }
