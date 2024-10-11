d3f7481d8238014a4ab45814fa07fa6837dcd99d
FusionSid <siddheshadsv@icloud.com>
2024-10-12 00:07:13 +1300

    feat: add function to get list of periods for the week

--- src/core/lib/times.ts
+++ src/core/lib/times.ts
@@ -1,6 +1,77 @@
+import prisma from "@db/orm";
+import { getWeek } from "./dates";
+import { WeekPeriods } from "@core/types/timetable";
+
 // converts 24h to 12h time
 export function formatTime(timeString: string) {
     const [hourString, minute] = timeString.split(":");
     const hour = +hourString % 24;
     return (hour % 12 || 12) + ":" + minute + (hour < 12 ? "AM" : "PM");
 }
+
+// get all the periods for the current week
+export async function getTimetable(date?: Date): Promise<WeekPeriods> {
+    if (!date) {
+        date = new Date();
+    }
+
+    const weekNumber = getWeek(date);
+    const currentYear = date.getFullYear();
+
+    const week = await prisma.week.findFirst({
+        where: { year: currentYear, number: weekNumber },
+        include: { weekTimetable: weekIncludeQuery },
+    });
+
+    let timetable;
+    if (week) {
+        timetable = week.weekTimetable;
+    } else {
+        timetable = await prisma.weekTimetable.findUnique({
+            where: { name: "Default" },
+            ...weekIncludeQuery,
+        });
+    }
+
+    if (!timetable) {
+        throw Error("Cannot Find Timetable");
+    }
+
+    return [
+        timetable.monday.periods,
+        timetable.tuesday.periods,
+        timetable.wednesday.periods,
+        timetable.thursday.periods,
+        timetable.friday.periods,
+    ];
+}
+
+const weekIncludeQuery = {
+    include: {
+        monday: {
+            include: {
+                periods: true,
+            },
+        },
+        tuesday: {
+            include: {
+                periods: true,
+            },
+        },
+        wednesday: {
+            include: {
+                periods: true,
+            },
+        },
+        thursday: {
+            include: {
+                periods: true,
+            },
+        },
+        friday: {
+            include: {
+                periods: true,
+            },
+        },
+    },
+} as const;
