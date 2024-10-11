824f2020931bc4af83d55f162966b2dde615c309
FusionSid <siddheshadsv@icloud.com>
2024-10-12 00:06:54 +1300

    feat: add types for week periods and timetable event

---
+++ src/core/types/timetable.ts
@@ -0,0 +1,21 @@
+import { Period } from "@prisma/client";
+
+export type WeekPeriods = [Period[], Period[], Period[], Period[], Period[]];
+
+export type TimetableEventProps = {
+    title: string | React.JSX.Element;
+    description: string | React.JSX.Element | null;
+    borderColor: string;
+    backgroundColor: string;
+    startTime: string;
+    endTime: string;
+} & (
+    | {
+          locked: true;
+      }
+    | {
+          locked: false;
+          booked: boolean;
+          url: string;
+      }
+);
