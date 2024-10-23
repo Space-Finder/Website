97805f46580569115a8400892308e594d44371a6
FusionSid <siddheshadsv@icloud.com>
2024-10-23 18:35:45 +1300

    chore: add pages boilerplate for admin

---
+++ src/app/admin/teachers/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TeachersPage = () => {
+    return <div>TeachersPage</div>;
+};
+
+export default TeachersPage;
---
+++ src/app/admin/timetable/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TimetablePage = () => {
+    return <div>TimetablePage</div>;
+};
+
+export default TimetablePage;
