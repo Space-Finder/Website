d8e98b926e4f07d14166bd3bc8430acf1b3789a7
FusionSid <siddheshadsv@icloud.com>
2024-10-23 22:31:20 +1300

    feat: add page to show digi screens images

---
+++ src/app/dashboard/screens/page.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+
+import prisma from "@db/orm";
+
+const Screens = async () => {
+    const commons = await prisma.common.findMany();
+
+    return (
+        <div className="m-4 grid grid-cols-2 items-center justify-center gap-4 md:grid-cols-4">
+            {commons.map((common) => {
+                return (
+                    <Image
+                        alt={common.name}
+                        key={common.id}
+                        width={1080}
+                        height={1920}
+                        className="h-auto w-full rounded-md shadow-lg"
+                        src={`${process.env.NEXT_PUBLIC_API_URL}/api/image?common_id=${common.id}`}
+                    />
+                );
+            })}
+        </div>
+    );
+};
+
+export default Screens;
